bdc612e25ed8d70d834215b269b690f808ce4bfd
SilencerWeb <silencer.web@gmail.com>
2019-01-06 16:29:54 +0500

    Set up project structure; Add .idea/ to .gitignore; Add .env; Add dependencies styled-components, react-dom-router and normalize.css

---
+++ src/pages/main.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import { Container } from 'ui/atoms';
+
+
+export const MainPage = (props) => {
+
+  return (
+    <div>
+      <Container>
+        Hello World!
+      </Container>
+    </div>
+  );
+};
